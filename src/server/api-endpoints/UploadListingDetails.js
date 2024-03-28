c7edf12cb993d81fbff4c2cb6c376e541bc1fed6
Wong Yu Le <YuLeWong2002@gmail.com>
2024-03-29 00:24:47 +0800

    Update the package

--- src/server/api-endpoints/UploadListingDetails.js
+++ src/server/api-endpoints/UploadListingDetails.js
@@ -4,9 +4,9 @@ const db = require('../database/connection.js');
 module.exports = async function uploadListingDetails(req, res) {
   console.log("IN uploadListingDetails backend");
 
-  const { title, description, price, category, ContactDescription, PostedBy } = req.body;
+  const { title, description, condition, price, category, ContactDescription, PostedBy } = req.body;
 
-  console.log("Received data from the request:", { title, description, price, category, ContactDescription, PostedBy });
+  console.log("Received data from the request:", { title, description, condition, price, category, ContactDescription, PostedBy });
 
   // Insert the new listing into the database
   const insertQuery = 'INSERT INTO listing (title, description, price, category, ContactDescription, PostedBy) VALUES (?, ?, ?, ?, ?, ?)';
