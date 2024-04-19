f6a8196d9b2b218df96fb1dbdd34894f99625602
JunKit2003 <chee.jk@gmail.com>
2024-04-20 01:53:10 +0800

    Database file updated

--- src/server/database/marketplace.sql
+++ src/server/database/marketplace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2024 at 04:30 PM
+-- Generation Time: Apr 19, 2024 at 07:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,6 +39,14 @@ CREATE TABLE `accounts` (
   `token` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `ProfilePicture`, `first_name`, `last_name`, `username`, `email`, `phone_number`, `password`, `token`) VALUES
+(19, '', 'test', 'test', 'test', 'test@test', '29340', '$2b$10$4IoEJF/cKiwr/4p2vUV3R.ePuoqHV6KfoyNdFtuFUSiTYKrH2idka', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidGVzdCJ9.fEphzGSvIaHPFvC8xyqTxs93fEuiOv7di5BYPOkuvvc'),
+(20, '', 'test2', 'test2', 'test2', 'test2@test', '2097450982', '$2b$10$LF.BijsgWkW1lf04aYIEI.E55AIMIfkdgCqUQjiHpWaCKf4XWhn5.', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidGVzdDIifQ.QeOUJoUHXtiZD3pIbiDLx-ZggeD6qQkvrJFDAhhO8Y4');
+
 -- --------------------------------------------------------
 
 --
@@ -59,6 +67,34 @@ CREATE TABLE `listing` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `listing`
+--
+
+INSERT INTO `listing` (`id`, `title`, `ImageID`, `condition`, `description`, `price`, `category`, `ContactDescription`, `postedDate`, `PostedBy`) VALUES
+(111, 'tool', '1713530356974.jpg', 'Brand new', 'tool', 2143.00, 'Home & Garden', '1234513242', '2024-04-19 12:39:16', 'test2'),
+(119, 'r', '1713544419342.jpg', 'Brand new', 'gre', 1233.00, 'Entertainment', 'Message me with the button below !', '2024-04-19 16:33:39', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved`
+--
+
+CREATE TABLE `saved` (
+  `Number` int(11) NOT NULL,
+  `SavedBy` varchar(255) NOT NULL,
+  `ListingID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `saved`
+--
+
+INSERT INTO `saved` (`Number`, `SavedBy`, `ListingID`) VALUES
+(8, 'test', 111),
+(9, 'test', 119);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -75,6 +111,12 @@ ALTER TABLE `listing`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `saved`
+--
+ALTER TABLE `saved`
+  ADD PRIMARY KEY (`Number`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -82,13 +124,19 @@ ALTER TABLE `listing`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `listing`
 --
 ALTER TABLE `listing`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+
+--
+-- AUTO_INCREMENT for table `saved`
+--
+ALTER TABLE `saved`
+  MODIFY `Number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
